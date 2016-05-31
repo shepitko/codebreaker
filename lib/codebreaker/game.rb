0a41f3afbe51c2069ce5c0653c713f21a0b0151c
shepitko <ya.shepitko@gmail.com>
2016-05-31 21:41:20 +0300

    fixed bug with hint

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -15,6 +15,7 @@ module Codebreaker
       @secret_code = (1..4).map{ rand(1..6) }.join
       @state = :new_game
       @cnt = 0
+      @hint = false
       @mode = MODES[:normal] 
     end
 
@@ -40,8 +41,14 @@ module Codebreaker
       code = @secret_code.chars
       num = num.chars
       mark = code.zip(num).delete_if{|v| v[0] == v[1]} 
+
       plus = '+' * (4 - mark.length)
-      minus = '-' * (mark.transpose[0] & mark.transpose[1]).length
+
+      minus = ""
+      one = mark.transpose[0].sort!
+      two = mark.transpose[1].sort!
+      (0..mark.length-1).each{ |index| minus += "-" if one[index] == two[index] }
+      
       result = plus << minus
     end
     
